dc4ac7a7f8bd9e24d00b297151b26a463e58c2a2
juozas.kontvainis <juozas@delfi.lt>
2015-04-12 19:20:39 +0300

    Sources and tests for index2d

---
+++ inc/index2d.h
@@ -0,0 +1,135 @@
+#include <cassert>
+#include <algorithm>
+
+namespace overengineered {
+
+
+template<class T>
+class index2d final
+{
+private:
+    T** data;
+    int minX, minY;
+    int sizeX, sizeY;
+    
+public:
+    index2d() : data{ nullptr } {}
+
+    void set(int x, int y, T* item)
+    {
+        if (item == nullptr) return;
+        ensure_assignable(x, y);
+
+        size_t index = (y - minY) * sizeY + (x - minX);
+        assert(index < capacity());
+        data[index] = item;
+    }
+
+    T* get(int x, int y)
+    {
+        if (data == nullptr) return nullptr;
+        if (x < minX || x >= minX + sizeX) return nullptr;
+        if (y < minY || y >= minY + sizeY) return nullptr;
+        size_t index = (y - minY) * sizeY + (x - minX);
+        assert(index < capacity());
+        return data[index];
+    }
+
+    size_t capacity()
+    {
+        return sizeX * sizeY;
+    }
+
+    void reserve(int minX, int maxX, int minY, int maxY)
+    {
+        int xmin = minX, xmax = maxX;
+        int ymin = minY, ymax = maxY;
+        if (data != nullptr)
+        {
+            xmin = std::min(xmin, this->minX);
+            xmax = std::max(xmax, this->minX + sizeX);
+            ymin = std::min(ymin, this->minY);
+            ymax = std::max(ymax, this->minY + sizeY);
+        }
+
+        resize(xmin, xmax, ymin, ymax);
+    }
+
+    template<typename Func>
+    void for_each(Func f)
+    {
+        if (data == nullptr) return;
+
+        int x = minX, y = minY, n = capacity(), xLimit = minX + sizeX;
+        for (int index = 0; index < n; index++)
+        {
+            if (data[index] != nullptr)
+                f(x, y, data[index]);
+            x++;
+            if (x == xLimit)
+            {
+                x = minX;
+                y++;
+            }
+        }
+    }
+
+private:
+    void ensure_assignable(int x, int y)
+    {
+        if (data == nullptr)
+        {
+            resize(x - 5, x + 5, y - 5, y + 5);
+            return;
+        }
+
+        int xmin = minX, xmax = minX + sizeX;
+        int ymin = minY, ymax = minY + sizeY;
+        bool expandX = true, expandY = true;
+
+        if (x < xmin) xmin = x - (sizeX / 3);
+        else if (x >= xmax) xmax = x + (sizeX / 3);
+        else expandX = false;
+
+        if (y < ymin) ymin = y - (sizeY / 3);
+        else if (y >= ymax) ymax = y + (sizeY / 3);
+        else expandY = false;
+
+        if (expandX || expandY) resize(xmin, xmax, ymin, ymax);
+    }
+
+    void resize(int xmin, int xmax, int ymin, int ymax)
+    {
+        assert(xmin < xmax);
+        assert(ymin < ymax);
+
+        auto oldData = data;
+        auto oldSizeX = sizeX;
+        auto overlapSizeX = std::max(0, std::min(sizeX, xmax - xmin) + std::min(minX - xmin, 0));
+        auto overlapSizeY = std::max(0, std::min(sizeY, ymax - ymin) + std::min(minY - ymin, 0));
+        auto sourceOffset = sizeX - overlapSizeX;
+        auto targetOffset = std::min(minX - xmin, 0);
+
+        sizeX = xmax - xmin;
+        sizeY = ymax - ymin;
+        minX = xmin;
+        minY = ymin;
+        auto length = sizeX * sizeY;
+        data = new T*[length];
+        memset(data, 0, length * sizeof(T*));
+
+        if (oldData == nullptr) return;
+
+        for (auto i = 0; i < overlapSizeY; i++)
+        {
+            memcpy(data + targetOffset, oldData + sourceOffset, overlapSizeX * sizeof(T*));
+            targetOffset += sizeX;
+            sourceOffset += oldSizeX;
+        }
+
+        delete[] oldData;
+    }
+};
+
+
+}
---
+++ tests/index2d_tests.cpp
@@ -0,0 +1,66 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../inc/index2d.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using overengineered::index2d;
+
+
+namespace Index2d_Tests
+{		
+    TEST_CLASS(Index2d_Tests)
+    {
+    public:
+        
+        TEST_METHOD(ItemAdded_CanBeRetrieved)
+        {
+            auto grid = index2d<double>{};
+            auto item = 3.14159;
+
+            grid.set(1, 1, &item);
+            Assert::AreEqual(&item, grid.get(1, 1));
+        }
+
+        TEST_METHOD(AddingAnItem_IncreasesCapacity)
+        {
+            auto grid = index2d<std::string>{};
+            auto item = std::string{ "hello" };
+            grid.set(-500, -500, &item);
+            Assert::IsTrue(grid.capacity() > 0);
+        }
+
+        TEST_METHOD(AccessingUnsetItem_ReturnsNull)
+        {
+            auto grid = index2d<uint8_t>{};
+            auto item = static_cast<uint8_t>(3);
+            grid.set(0, 1, &item);
+            grid.set(1, 0, &item);
+            Assert::IsTrue(nullptr == grid.get(0, 0));
+        }
+
+        TEST_METHOD(AddedItems_CanBeIterated)
+        {
+            const auto itemCount = 100;
+            auto grid = index2d<int>{};
+
+            int buffer[itemCount];
+            for (auto i = 0; i < itemCount; i++)
+            {
+                buffer[i] = i;
+                grid.set(i, i, buffer + i);
+            }
+
+            auto total = 0;
+            grid.for_each([&total, &grid](int x, int y, int* item)
+            {
+                Assert::AreEqual(x, *item);
+                Assert::AreEqual(y, *item);
+                Assert::AreEqual(grid.get(total, total), item);
+                total++;
+            });
+
+            Assert::AreEqual(itemCount, total);
+        }
+
+    };
+}
